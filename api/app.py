b9ea61a42175604fb37661776b544d1c0b0f5f17
Soham Kulkarni <sohamkrox@gmail.com>
2022-10-12 23:11:54 -0500

    Add more docs and hints to app.py

--- api/app.py
+++ api/app.py
@@ -1,60 +1,71 @@
-from flask import Flask, request
+from http.client import BAD_REQUEST, OK
+from flask import Flask, abort, request
 from flask_cors import CORS
 
 events = {
-        0 : {
-            "id" : 0,
-            "name" : "event1",
-            "category" : "food", 
-            "location" : "alma",
-            "date" : "UTC ",
-        }, 
-        1 : {
-            "id" : 1,
-            "name" : "event2",
-            "category" : "food", 
-            "date" : "UTC ",
-            "location" : "alma",
-        },
-        2 : {
-            "id" : 2,
-            "name" : "event3",
-            "category" : "food",
-            "location" : "alma", 
-            "date" : "UTC ",
-        }}
+    # Here is an example event:
+    0: {
+        "id": 0,
+        "name": "OSAI Social",
+        "category": "food",
+        "location": "CIF 1038",
+        "date": "Oct 6th 2022 7PM",
+    }
+}
 
 app = Flask(__name__)
 CORS(app)
 
-@app.get("/")
+
+@ app.get("/")
 def get_default():
-    return {"message": "Welcome to OSAI"}
+    return {"message": "I <3 Open-Source at Illinois"}
 
-@app.get("/all")
+
+@ app.get("/all")
 def get_all():
+    """
+    Returns a map of all events to their event ID
+    """
     # Your code goes here
-    return "todo"
 
-@app.get("/event/<event_id>")
+    # Since this function is not yet implemented, return Status 501
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/501
+    abort(501)
+
+
+@ app.get("/event/<event_id>")
 def get_event(event_id: int):
+    """
+    Returns the event object for the given event_id
+    """
+    event_id = int(event_id)
     # Your code goes here
-    return "todo"
 
-@app.post("/event")
+    abort(501)
+
+
+@ app.post("/event")
 def new():
+    """
+    Stores a new event object in the database
+    """
+    # HINT: You can access the object to be created using request.json
     # Your code goes here
-    return "todo"
 
-@app.put("/event/<event_id>")
-def update(event_id: int):
-    # Your code goes here
-    return "todo"   
+    abort(501)
 
-@app.delete("/event/<event_id>")
+
+@ app.delete("/event/<event_id>")
 def delete(event_id: int):  # no error handling
+    """
+    Deletes the event object for the given event_id
+    """
+    event_id = int(event_id)
     # Your code goes here
-    return "todo"
+
+    abort(501)
+
 
 if __name__ == '__main__':
     app.run()
